9392ef675108d71f3d34da93990972db986698bc
Emre Karayaman <karayaman.emre@outlook.com>
2022-09-19 14:05:20 +0300

    created functions
    - Created a collection that keeps a list of books.
    - Created a function to add a new book to the collection, with title and author.
    -Created a function to remove a book from the collection.

---
+++ script.js
@@ -0,0 +1,54 @@
+const addBtn = document.getElementById('add');
+const booksList = [];
+
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  const book = {
+    Title: title.value,
+    Author: author.value,
+  };
+  const list = document.getElementById('book-list');
+  const addDiv = document.createElement('div');
+  addDiv.classList.add('book');
+  booksList.push(book);
+  localStorage.setItem('books', JSON.stringify(booksList));
+  addDiv.innerHTML += `
+  <p>${book.Title}</p>
+  <p>${book.Author}</p>
+  <button class="delete button">Remove</button>
+  <hr>
+  `;
+  list.appendChild(addDiv);
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+});
+
+document.getElementById('book-list').addEventListener('click', (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains('delete')) {
+    e.target.parentElement.remove();
+  }
+  booksList.forEach((book, i) => {
+    if (e.target.parentElement.firstElementChild.textContent === book.Title) {
+      booksList.splice(i, 1);
+    }
+  });
+  localStorage.setItem('books', JSON.stringify(booksList));
+});
+
+window.addEventListener('load', () => {
+  const stores = JSON.parse(localStorage.getItem('books'));
+  stores.forEach((store) => {
+    const addDiv = document.createElement('div');
+    addDiv.innerHTML += `
+    <p>${store.Title}</p>
+    <p>${store.Author}</p>
+    <button class="delete button">Remove</button>
+    <hr>      
+    `;
+    const list = document.getElementById('book-list');
+    list.appendChild(addDiv);
+  });
+});
